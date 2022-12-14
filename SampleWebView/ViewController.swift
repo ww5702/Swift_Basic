789a69445b549ec629ddaa7028074b41eebf97fd
ios <wow5702@naver.com>
2022-12-14 16:57:15 +0900

    WebViewTest

--- SampleWebView/ViewController.swift
+++ SampleWebView/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var WebViewMain: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // url string을 준비한다
+        // url 주소를 URLRequest로 만든다
+        // req >> load
+        let urlString = "https://www.google.com"
+        if let url = URL(string: urlString) {
+            let urlReq = URLRequest(url: url)
+            WebViewMain.load(urlReq)
+        }
     }
-
-
 }
 
